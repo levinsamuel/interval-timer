d142490a123bb34017f5312f7e619d7915029a26
Samuel Levin <levinsamuel000@gmail.com>
2020-01-26 19:16:04 -0800

    dont commit lib

--- fun-timer/lib/Timer.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare type PropTypes = {
-    seconds?: number;
-    minutes?: number;
-    started?: boolean;
-    className?: string;
-    beepInterval?: number;
-};
-export default function Timer(props: PropTypes): JSX.Element;
